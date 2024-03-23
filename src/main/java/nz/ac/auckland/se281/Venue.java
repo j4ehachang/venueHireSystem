60ebe5be3c1d4e0ae3ffaba26806899af5ff1b6f
Jaeha <jcha513@aucklanduni.ac.nz>
2024-03-23 17:41:14 +1300

    Created new class and constructor for venues

---
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -0,0 +1,18 @@
+package nz.ac.auckland.se281;
+
+public class Venue {
+  private String venueName;
+  private String venueCode;
+  private String capacityInput;
+  private String hireFeeInput;
+
+  public Venue (String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+    this.venueName = venueName;
+    this.venueCode = venueCode;
+    this.capacityInput = capacityInput;
+    this.hireFeeInput =  hireFeeInput;
+  }
+
+  
+
+}

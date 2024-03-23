a90a1d15f7f2549d916b1329014ae14ff14fb2a6
Jaeha <jcha513@aucklanduni.ac.nz>
2024-03-23 20:55:03 +1300

    Prints out the venue details when there is one venue in the list

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -37,6 +37,10 @@ public class VenueHireSystem {
       MessageCli.NO_VENUES.printMessage();
     } else if (venueList.size() == 1) {
       MessageCli.NUMBER_VENUES.printMessage("is", "one", "");
+      MessageCli.VENUE_ENTRY.printMessage(venueList.get(0).get_venueName(), 
+      venueList.get(0).get_venueCode(), venueList.get(0).get_capacityInput(), 
+      venueList.get(0).get_hireFeeInput());
+
     } else if (venueList.size() < 10) {
       MessageCli.NUMBER_VENUES.printMessage("are", switchCase(venueList.size()), "s");
     } else if (venueList.size() >= 10) {

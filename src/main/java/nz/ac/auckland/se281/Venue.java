b0f301fb5fdf8c4c598927f58f6a286d94a7b6e8
Jaeha <jcha513@aucklanduni.ac.nz>
2024-04-02 21:11:11 +1300

    Updated code format and added comments for easier understading

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -6,26 +6,26 @@ public class Venue {
   private String capacityInput;
   private String hireFeeInput;
 
-  public Venue (String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+  public Venue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     this.venueName = venueName;
     this.venueCode = venueCode;
     this.capacityInput = capacityInput;
-    this.hireFeeInput =  hireFeeInput;
+    this.hireFeeInput = hireFeeInput;
   }
 
-  public String get_venueName(){
+  public String get_venueName() {
     return this.venueName;
-  }  
+  }
 
-  public String get_venueCode(){
+  public String get_venueCode() {
     return this.venueCode;
-  }  
+  }
 
-  public String get_capacityInput(){
+  public String get_capacityInput() {
     return this.capacityInput;
-  }  
+  }
 
-  public String get_hireFeeInput(){
+  public String get_hireFeeInput() {
     return this.hireFeeInput;
-  }  
+  }
 }
--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -1,16 +1,17 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
-import java.util.ArrayList;
 
 public class VenueHireSystem {
   public VenueHireSystem() {}
+
   private ArrayList<Venue> venueList = new ArrayList<>();
-  
-  private String integerToString(int size){
-    
-    switch(size){
+
+  private String integerToString(int size) {
+    // Return corresponding integer in the format of a string
+    switch (size) {
       case 2:
         return "two";
       case 3:
@@ -33,67 +34,88 @@ public class VenueHireSystem {
   }
 
   public void printVenues() {
-    if (venueList.isEmpty()) { 
+    // If there are no venues in the system tell the user to create a venue first
+    if (venueList.isEmpty()) {
       MessageCli.NO_VENUES.printMessage();
+
+      // If there is one venue in the system print out that there is one venue
     } else if (venueList.size() == 1) {
       MessageCli.NUMBER_VENUES.printMessage("is", "one", "");
+      // Print out the details of the venues in the system
       for (Venue venue : venueList) {
-        MessageCli.VENUE_ENTRY.printMessage(venue.get_venueName(), venue.get_venueCode(), venue.get_capacityInput(), venue.get_hireFeeInput());
+        MessageCli.VENUE_ENTRY.printMessage(
+            venue.get_venueName(),
+            venue.get_venueCode(),
+            venue.get_capacityInput(),
+            venue.get_hireFeeInput());
       }
-
+      // If there are more than one and less than ten venues print the number of venues as a string
     } else if (venueList.size() < 10 && venueList.size() > 1) {
       MessageCli.NUMBER_VENUES.printMessage("are", integerToString(venueList.size()), "s");
       for (Venue venue : venueList) {
-        MessageCli.VENUE_ENTRY.printMessage(venue.get_venueName(), venue.get_venueCode(), venue.get_capacityInput(), venue.get_hireFeeInput());
+        MessageCli.VENUE_ENTRY.printMessage(
+            venue.get_venueName(),
+            venue.get_venueCode(),
+            venue.get_capacityInput(),
+            venue.get_hireFeeInput());
       }
-
+      // If there are more than or equal to ten venues print out the number of venues as a integer
     } else if (venueList.size() >= 10) {
       MessageCli.NUMBER_VENUES.printMessage("are", String.valueOf(venueList.size()), "s");
       for (Venue venue : venueList) {
-        MessageCli.VENUE_ENTRY.printMessage(venue.get_venueName(), venue.get_venueCode(), venue.get_capacityInput(), venue.get_hireFeeInput());
+        MessageCli.VENUE_ENTRY.printMessage(
+            venue.get_venueName(),
+            venue.get_venueCode(),
+            venue.get_capacityInput(),
+            venue.get_hireFeeInput());
       }
     }
   }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-      Venue newVenue = new Venue(venueName, venueCode, capacityInput, hireFeeInput);
-      
-      try {
+    Venue newVenue = new Venue(venueName, venueCode, capacityInput, hireFeeInput);
+
+    // Return error message if the capacity given by the user is negative or not a number
+    try {
       int capacity = Integer.parseInt(capacityInput);
       if (capacity < 0) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
         return;
       }
-      } catch(Exception e){
-        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "");
+    } catch (Exception e) {
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "");
+      return;
+    }
+
+    // Return error message if the hire fee given by the user is negative or not a number
+    try {
+      int hireFee = Integer.parseInt(hireFeeInput);
+      if (hireFee < 0) {
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
         return;
       }
+    } catch (Exception e) {
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
+      return;
+    }
 
-      try {
-        int hireFee = Integer.parseInt(hireFeeInput);
-        if (hireFee < 0) {
-          MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
-          return;
-        }
-        } catch(Exception e){
-          MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
-          return;
-        }
-
-      for (Venue venue : venueList) {
-        if (venue.get_venueCode().equals(venueCode)){
-          MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue.get_venueName());
-          return;
-        }
+    // Return error message if the venue code given by the user already exists for another venue
+    for (Venue venue : venueList) {
+      if (venue.get_venueCode().equals(venueCode)) {
+        MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue.get_venueName());
+        return;
       }
+    }
 
-      if (venueName.isEmpty()) {
-        MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
-      } else {
-        MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
-        venueList.add(newVenue);
-      }
+    // Return error message if the venue name is not given
+    if (venueName.isEmpty()) {
+      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+    } else {
+      // If no errors were found create venue and add to the arraylist of venues
+      MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
+      venueList.add(newVenue);
+    }
   }
 
   public void setSystemDate(String dateInput) {

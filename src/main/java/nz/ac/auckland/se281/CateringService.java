125f992b9341c9e86b250f258ef0a4f598512010
Jaeha <jcha513@aucklanduni.ac.nz>
2024-04-05 17:58:33 +1300

    Updated formatting for catering and music service classes to improve code style

--- src/main/java/nz/ac/auckland/se281/CateringService.java
+++ src/main/java/nz/ac/auckland/se281/CateringService.java
@@ -1,9 +1,8 @@
 package nz.ac.auckland.se281;
 
-public class CateringService extends Service{
+public class CateringService extends Service {
 
   public CateringService(String bookingReference, String typeName, int serviceFee) {
     super(bookingReference, typeName, serviceFee);
   }
-
 }
--- src/main/java/nz/ac/auckland/se281/MusicService.java
+++ src/main/java/nz/ac/auckland/se281/MusicService.java
@@ -1,7 +1,7 @@
 package nz.ac.auckland.se281;
 
-public class MusicService extends Service{
-  
+public class MusicService extends Service {
+
   public MusicService(String bookingReference, String typeName, int serviceFee) {
     super(bookingReference, typeName, serviceFee);
   }
